78c15b0d75564571269609ab9672f73d3f1e03d7
Tony Toscano <atoscano@holts.com>
2022-02-22 12:04:47 -0500

    Added babel-core and babel-loader

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,16 +1,37 @@
 const { mode } = require('webpack-nano/argv');
-const path = require('path');
+const paths = require('./paths');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
-	mode,
-	output: {
-		filename: '[name].bundle.js',
-		path: path.resolve(__dirname, '../dist')
-	},
-	plugins: [
-		new HtmlWebpackPlugin()
-	]
-}
+  entry: [paths.src + '/index.js'],
+  mode,
+  output: {
+    filename: '[name].bundle.js',
+    path: paths.build,
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  resolve: {
+    modules: [paths.src, 'node_modules'],
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      '@': paths.src,
+      assets: paths.public,
+    },
+  },
+};
--- src/component.js
+++ src/component.js
@@ -1,5 +1,5 @@
 export default (text = 'Hello Webpack') => {
-	const element = document.createElement('div');
-	element.innerHTML = text;
-	return element;
+  const element = document.createElement('div');
+  element.innerHTML = text;
+  return element;
 };
